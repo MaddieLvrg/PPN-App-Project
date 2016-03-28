30a1f3aca638479f0e185b5e28a7e5eb0f4febdc
MaddieLvrg <m_lavergne_92@msn.com>
2016-03-28 14:35:45 -0500

    Layouts and the rest
    The rest of the site.

--- index.php
+++ index.php
@@ -2,35 +2,43 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-#$loader = new Twig_Loader_Filesystem('views');
-#$twig = new Twig_Environment($loader);
-
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/views'));
-
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array('dbname' => 'ppnblogdb'),
+));
 
 $app['debug'] = true;
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
         return sprintf('%s/%s', trim($app['request']->getBasePath()), ltrim($asset, '/'));
     }));
+    $twig->addGlobal('posts', $app['db']->fetchAll('SELECT * FROM posts'));
     return $twig;
 }));
 
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-#sets global value of active path
-$app->before(function ($request) use ($app) {
-    $app['twig']->addGlobal('active', $request->get("_route"));
+$app->get('/post/{id}', function ($id) use ($app) {
+    $sql = "SELECT * FROM posts WHERE id = ? ORDER BY date ASC";
+    $post = $app['db']->fetchAssoc($sql, array($id));
+    return $app['twig']->render('post.twig', [
+        'post' => $post
+    ]);
+});
+
+$app->get('/archive/{year}/{month}', function($year, $month) use ($app) {
+    $sql = "SELECT * FROM posts WHERE YEAR(date) = ? AND MONTH(date) = ? ORDER BY date ASC";
+    $archivePosts = $app['db']->fetchAll($sql, array($year, $month));
+    return $app['twig']->render('archive.twig', [
+    'archivePosts' => $archivePosts, 'selectedYear' => $year, 'selectedMonth' => $month
+    ]);
 });
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('layout.twig');
 })->bind('home');
 
-$app->get('/archive', function() use ($app) {
-    return $app['twig']->render('archive.twig');
-})->bind('archive');
 
 $app->run();
 
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -13,4 +13,6 @@ return array(
     'Symfony\\Component\\Debug\\' => array($vendorDir . '/symfony/debug'),
     'Symfony\\Bridge\\Twig\\' => array($vendorDir . '/symfony/twig-bridge'),
     'Silex\\' => array($vendorDir . '/silex/silex/src/Silex'),
+    'Doctrine\\Common\\Cache\\' => array($vendorDir . '/doctrine/cache/lib/Doctrine/Common/Cache'),
+    'Doctrine\\Common\\' => array($vendorDir . '/doctrine/common/lib/Doctrine/Common'),
 );
